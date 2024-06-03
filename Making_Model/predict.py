a7ae15201504386b0bca90f44a2eda64c381eef2
tlgusdl03 <tlgusdl03@gmail.com>
2024-06-03 12:24:05 +0900

    feat : 쓰레기 배출량 데이터 정규화 진행, 3개로 범주화하여 classification 문제로 변환

--- Making_Model/predict.py
+++ Making_Model/predict.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeRegressor, plot_tree
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.tree import DecisionTreeRegressor, plot_tree, DecisionTreeClassifier
+from sklearn.metrics import mean_squared_error, r2_score, accuracy_score
 import matplotlib.pyplot as plt
 
 
@@ -15,7 +15,7 @@ def remove_outliers(df, column):
     return df[(df[column] >= lower_bound) & (df[column] <= upper_bound)]
 
 
-data = pd.read_csv("/after_data.csv")
+data = pd.read_csv("C:\\Users\\tlgus\Desktop\ML_alternative\pythonProject1\\after_data.csv")
 
 data.drop(columns=['Unnamed: 0', 'MIN_L', 'MIN_H', 'MAX_L', 'MAX_H'], inplace=True)
 data['date'] = pd.to_datetime(data['date'])
@@ -23,28 +23,34 @@ data.set_index('date', inplace=True)
 
 # 이상치 제거
 clean_data = remove_outliers(data, 'dscamt')
-clean_data = remove_outliers(clean_data, 'RN_ST')
+#clean_data = remove_outliers(clean_data, 'RN_ST')
+
+# 데이터 범주화
+clean_data['dscamt_normalized'] = pd.qcut(clean_data['dscamt'], 3, labels=False)
 
 # 특성과 타겟 변수 분리
-X_clean = clean_data.drop('dscamt', axis=1)
-y_clean = clean_data['dscamt']
+# X_clean = clean_data.drop('dscamt', axis=1)
+X_clean = clean_data[['MIN', 'MAX', 'RN_ST']]
+y_clean = clean_data['dscamt_normalized']
 
 # 데이터를 훈련 세트와 테스트 세트로 분리
 X_train_clean, X_test_clean, y_train_clean, y_test_clean = train_test_split(X_clean, y_clean, test_size=0.2, random_state=42)
 
 # 의사결정트리 모델 생성 및 훈련 (가지치기 적용)
-pruned_tree_model = DecisionTreeRegressor(max_depth=5, random_state=42)
+pruned_tree_model = DecisionTreeClassifier(max_depth=5, random_state=42)
 pruned_tree_model.fit(X_train_clean, y_train_clean)
 
 # 모델 예측
 y_pred_pruned_tree = pruned_tree_model.predict(X_test_clean)
+accuracy = accuracy_score(y_test_clean, y_pred_pruned_tree)
+print("Accuracy:", accuracy)
 
-# 모델 성능 평가
-mse_pruned_tree = mean_squared_error(y_test_clean, y_pred_pruned_tree)
-r2_pruned_tree = r2_score(y_test_clean, y_pred_pruned_tree)
-
-print(f'MSE: {mse_pruned_tree}')
-print(f'R²: {r2_pruned_tree}')
+# # 모델 성능 평가
+# mse_pruned_tree = mean_squared_error(y_test_clean, y_pred_pruned_tree)
+# r2_pruned_tree = r2_score(y_test_clean, y_pred_pruned_tree)
+#
+# print(f'MSE: {mse_pruned_tree}')
+# print(f'R²: {r2_pruned_tree}')
 
 # 의사결정 트리 시각화
 fig, ax = plt.subplots(figsize=(100, 20))
