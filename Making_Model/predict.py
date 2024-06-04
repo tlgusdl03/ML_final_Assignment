67760a38d15672dcfeb2a9a8fa545db9e80b1a03
tlgusdl03 <tlgusdl03@gmail.com>
2024-06-04 11:52:27 +0900

    feat : linear_regression 테스트 결과 추가

--- Making_Model/predict.py
+++ Making_Model/predict.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor, plot_tree, DecisionTreeClassifier
 from sklearn.metrics import mean_squared_error, r2_score, accuracy_score
@@ -25,25 +26,37 @@ data.set_index('date', inplace=True)
 clean_data = remove_outliers(data, 'dscamt')
 #clean_data = remove_outliers(clean_data, 'RN_ST')
 
-# 데이터 범주화
-clean_data['dscamt_normalized'] = pd.qcut(clean_data['dscamt'], 3, labels=False)
+# # 데이터 범주화
+# clean_data['dscamt_normalized'] = pd.qcut(clean_data['dscamt'], 3, labels=False)
 
 # 특성과 타겟 변수 분리
 # X_clean = clean_data.drop('dscamt', axis=1)
-X_clean = clean_data[['MIN', 'MAX', 'RN_ST']]
-y_clean = clean_data['dscamt_normalized']
+X_clean = clean_data[['PRE', 'WF', 'RN_ST', 'MIN', 'MAX']]
+y_clean = clean_data['dscamt']
 
 # 데이터를 훈련 세트와 테스트 세트로 분리
 X_train_clean, X_test_clean, y_train_clean, y_test_clean = train_test_split(X_clean, y_clean, test_size=0.2, random_state=42)
 
-# 의사결정트리 모델 생성 및 훈련 (가지치기 적용)
-pruned_tree_model = DecisionTreeClassifier(max_depth=5, random_state=42)
-pruned_tree_model.fit(X_train_clean, y_train_clean)
 
-# 모델 예측
-y_pred_pruned_tree = pruned_tree_model.predict(X_test_clean)
-accuracy = accuracy_score(y_test_clean, y_pred_pruned_tree)
-print("Accuracy:", accuracy)
+# 릿지 회귀 모델 생성 및 훈련
+ridge_model = Ridge(alpha=1.0)
+ridge_model.fit(X_train_clean, y_train_clean)
+
+# # 의사결정트리 모델 생성 및 훈련 (가지치기 적용)
+# pruned_tree_model = DecisionTreeClassifier(max_depth=5, random_state=42)
+# pruned_tree_model.fit(X_train_clean, y_train_clean)
+
+# # 의사결정 트리 모델 예측
+# y_pred_pruned_tree = pruned_tree_model.predict(X_test_clean)
+# accuracy = accuracy_score(y_test_clean, y_pred_pruned_tree)
+# print("Accuracy:", accuracy)
+
+# 모델 예측 및 평가
+y_pred = ridge_model.predict(X_test_clean)
+mse = mean_squared_error(y_test_clean, y_pred)
+r2 = r2_score(y_test_clean, y_pred)
+print("Mean Squared Error:", mse)
+print("R_2 : ", r2)
 
 # # 모델 성능 평가
 # mse_pruned_tree = mean_squared_error(y_test_clean, y_pred_pruned_tree)
@@ -52,9 +65,18 @@ print("Accuracy:", accuracy)
 # print(f'MSE: {mse_pruned_tree}')
 # print(f'R²: {r2_pruned_tree}')
 
-# 의사결정 트리 시각화
-fig, ax = plt.subplots(figsize=(100, 20))
-plot_tree(pruned_tree_model, feature_names=X_clean.columns, filled=True, ax=ax, fontsize=10)
-fig.savefig('decision_tree.png', dpi=300)
-plt.close(fig)
+# # 의사결정 트리 시각화
+# fig, ax = plt.subplots(figsize=(100, 20))
+# plot_tree(pruned_tree_model, feature_names=X_clean.columns, filled=True, ax=ax, fontsize=10)
+# fig.savefig('decision_tree.png', dpi=300)
+# plt.close(fig)
 
+# 결과 시각화
+plt.figure(figsize=(10, 6))
+plt.scatter(y_test_clean, y_pred, color='blue')
+plt.plot([y_test_clean.min(), y_test_clean.max()], [y_test_clean.min(), y_test_clean.max()], 'k--', lw=4)  # 대각선 추가
+plt.xlabel('Actual')
+plt.ylabel('Predicted')
+plt.title('Actual vs. Predicted')
+plt.savefig('linear_regression.png', dpi=300)
+plt.close()
